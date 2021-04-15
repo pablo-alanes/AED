65e9291e0bfba5dc7189e840ec43efaea8547aee
89912006 <alanespablo@gmail.com>
2021-04-14 21:35:52 -0300

    HelloWord: Agrego hello.cpp

---
+++ 00-CppHelloWorld/hello.cpp
@@ -0,0 +1,13 @@
+/* hello.cpp
+ * Programa que envíe a cout la línea "Hello, World!"
+ * Pablo Alanes
+ * 14-04-2021
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
